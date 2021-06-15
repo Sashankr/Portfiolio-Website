814cbe62e11396e994068d24172762c5cd5902a6
Sashankr <sashankr97@gmail.com>
2021-06-15 20:36:44 +0530

    Fixed responsive overflow issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,17 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Sashank Rampalli`,
+        short_name: `Sashank`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
   ],
 }
